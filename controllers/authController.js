3d347ba1e3761941ae9f18d065df1747caf6c9b1
Sujoy <sujoykr8@gmail.com>
2024-07-31 10:38:03 +0600

    add email verification

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,12 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const prisma = require('../config/db')
+
 const {userValidator} = require("../util/userValidator")
+const {sendConfirmationEmail} = require("../util/mailService")
+const {redisClient} = require("../config/redis");
+const path = require("path");
+const fs = require("fs");
 
 const register = async (req, res) => {
     const {email, password} = req.body
@@ -10,13 +15,25 @@ const register = async (req, res) => {
             await userValidator.validateAsync({email, password})
 
             const hashedPass = await bcrypt.hash(password, parseInt(process.env.SALT_ROUND, 10))
-            await prisma.user.create(
+            const user = await prisma.user.create(
                 {
                     data: {email, password: hashedPass}
                 }
             )
 
-            res.status(201).json({message: "Registration Successful"})
+            const confirmationToken = jwt.sign({userId: user.id}, process.env.JWT_SECRET)
+
+            await sendConfirmationEmail(email, confirmationToken)
+            await prisma.user.update({
+                where: {
+                    id: user.id
+                },
+                data: {
+                    confirmationToken
+                },
+            })
+
+            res.status(201).json({message: "Registration Successful. Please Check Your Email to Activate Your Account."})
 
         } catch (err) {
             // joi error handling
@@ -65,4 +82,58 @@ const login = async (req, res) => {
     }
 }
 
-module.exports = {register, login}
+const confirm = async (req, res) => {
+    try {
+        const token = req.params.token
+
+        if (!token) {
+            return res.status(400).json({message: 'No token provided'});
+        }
+
+        const data = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (!data.userId) {
+            return res.status(404).json({message: 'Not a Valid Token'});
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id: data.userId
+            }
+        })
+
+        if (!user) {
+            return res.status(404).json({message: 'Invalid Confirmation Token'});
+        }
+
+        if (user.emailConfirmed && !user.confirmationToken) {
+            return res.status(400).json({message: 'Account Already Verified'});
+        }
+
+        if (token !== user.confirmationToken) {
+            return res.status(400).json({message: 'Invalid Confirmation Token'});
+        }
+
+        await prisma.user.update({
+            where: {
+                id: user.id
+            },
+            data: {
+                emailConfirmed: true,
+                confirmationToken: null
+            },
+        })
+
+        return res.status(200).json({message: 'Email successfully verified!'});
+    } catch (err) {
+        console.log(err)
+        if (err.name === 'TokenExpiredError') {
+            // Handle expired token
+            return res.status(400).json({message: 'Expired link'});
+        }
+
+        res.status(500).json({message: "Unexpected Server Error"})
+    }
+}
+
+module.exports = {register, login, confirm}
---
+++ util/mailService.js
@@ -0,0 +1,31 @@
+const nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.NODEMAILER_EMAIL,
+        // user app password in gmail
+        pass: process.env.NODEMAILER_PASS,
+    }
+})
+
+const sendConfirmationEmail = async (to, code) => {
+    const mailOptions = {
+        from: process.env.NODEMAILER_EMAIL,
+        to,
+        subject: 'Your Confirmation Token',
+        text: `Click the link to activate your account: ${process.env.HOST_LINK}/api/auth/confirm/${code}`
+    }
+
+    try {
+        await transporter.sendMail(mailOptions)
+        console.log('email sent')
+    } catch (err) {
+        console.log('nodemailer error', err)
+    }
+}
+
+module.exports = {sendConfirmationEmail}
+
